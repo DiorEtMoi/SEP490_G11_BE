cfa34d2f4183fdf60ef78ccef86239e176804d8c
DiorEtMoi <dinhhoan0511@gmail.com>
2024-06-17 19:14:33 +0700

    Update create delete employee

--- src/main/java/com/restaurent/manager/service/IEmployeeService.java
+++ src/main/java/com/restaurent/manager/service/IEmployeeService.java
@@ -1,8 +1,17 @@
 package com.restaurent.manager.service;
 
-import com.restaurent.manager.dto.request.EmployeeRequest;
+import com.restaurent.manager.dto.request.employee.EmployeeRequest;
+import com.restaurent.manager.dto.request.employee.EmployeeUpdateRequest;
 import com.restaurent.manager.dto.response.EmployeeResponse;
+import com.restaurent.manager.entity.Employee;
+
+import java.util.List;
 
 public interface IEmployeeService {
     EmployeeResponse createEmployee(EmployeeRequest request);
+    EmployeeResponse updateEmployee(EmployeeUpdateRequest request);
+    Employee findEmployeeById(Long id);
+    EmployeeResponse findEmployeeByIdConvertDTO(Long id);
+    void deleteEmployee(Long id);
+    List<EmployeeResponse> findEmployeesByAccountId(Long accountId);
 }
