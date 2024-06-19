960fd7efd5287a23b2296e2fcef047c3891a23b0
nguyen.tuananh <nguyen.tuananh@petronas.com>
2024-06-19 14:56:39 +0700

    Thang commit features package Customer

--- src/main/java/com/restaurent/manager/service/impl/CustomerService.java
+++ src/main/java/com/restaurent/manager/service/impl/CustomerService.java
@@ -14,10 +14,7 @@ import com.restaurent.manager.service.ICustomerService;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
