98346ab6bfcdff247998b64d77eeb42f4ef9b4ae
DiorEtMoi <dinhhoan0511@gmail.com>
2024-07-23 15:30:42 +0700

    create bill function

--- src/main/java/com/restaurent/manager/dto/response/BillResponse.java
+++ src/main/java/com/restaurent/manager/dto/response/BillResponse.java
@@ -2,9 +2,10 @@ package com.restaurent.manager.dto.response;
 
 import com.restaurent.manager.dto.response.order.OrderResponse;
 import com.restaurent.manager.enums.MethodPayment;
+import lombok.Data;
 
 import java.time.LocalDate;
-
+@Data
 public class BillResponse {
     Long id;
     OrderResponse order;
