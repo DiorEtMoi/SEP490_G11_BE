82cf040c6ab760afa1d28cfc251258dc10248573
DiorEtMoi <dinhhoan0511@gmail.com>
2024-07-23 15:13:14 +0700

    create bill function

--- src/main/java/com/restaurent/manager/dto/request/BillRequest.java
+++ src/main/java/com/restaurent/manager/dto/request/BillRequest.java
@@ -1,7 +1,9 @@
 package com.restaurent.manager.dto.request;
 
 import com.restaurent.manager.enums.MethodPayment;
+import lombok.Data;
 
+@Data
 public class BillRequest {
     double total;
     MethodPayment methodPayment;
