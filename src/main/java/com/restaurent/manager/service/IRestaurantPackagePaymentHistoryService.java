2ccc7625fb780ed280b1ce1a2e804c21cd8e6ee3
DiorEtMoi <dinhhoan0511@gmail.com>
2024-08-05 22:55:14 +0700

    adding permission for end points

--- src/main/java/com/restaurent/manager/service/IRestaurantPackagePaymentHistoryService.java
+++ src/main/java/com/restaurent/manager/service/IRestaurantPackagePaymentHistoryService.java
@@ -5,5 +5,5 @@ import com.restaurent.manager.dto.request.RestaurantPackagePaymentHistoryRequest
 public interface IRestaurantPackagePaymentHistoryService {
 Long createRestaurantPackagePaymentHistory(RestaurantPackagePaymentHistoryRequest request);
 Long getNewId();
-void updateRestaurantPackagePaymentHistory(Long id);
+void updateRestaurantPackagePaymentHistory(Long id, RestaurantPackagePaymentHistoryRequest request);
 }
