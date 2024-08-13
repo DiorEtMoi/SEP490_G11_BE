ed4fcd241fe8568e259f70fa57231d620ea3affb
DiorEtMoi <dinhhoan0511@gmail.com>
2024-08-13 17:12:38 +0700

    update area

--- src/main/java/com/restaurent/manager/dto/request/Customer/CustomerRequest.java
+++ src/main/java/com/restaurent/manager/dto/request/Customer/CustomerRequest.java
@@ -3,6 +3,7 @@ package com.restaurent.manager.dto.request.Customer;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -17,6 +18,7 @@ public class CustomerRequest {
      @Valid
      @NotNull(message = "INVALID_PHONENUMBER")
      @NotBlank(message = "INVALID_PHONENUMBER")
+     @Pattern(regexp = "^(0[3|5|7|8|9])[0-9]{8}$", message = "INVALID_PHONENUMBER")
      String phoneNumber;
 
      @NotNull(message = "INVALID_CUSTOMER_NAME")
