520dfd5a24897232869b52439b306f7ee199f4d7
DiorEtMoi <dinhhoan0511@gmail.com>
2024-08-21 03:13:09 +0700

    update dish and paging search for customer

--- src/main/java/com/restaurent/manager/dto/request/dish/DishRequest.java
+++ src/main/java/com/restaurent/manager/dto/request/dish/DishRequest.java
@@ -10,19 +10,22 @@ import lombok.experimental.FieldDefaults;
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class DishRequest {
-    @NotNull(message = "NOT_EMPTY")
-    @NotBlank(message = "NOT_EMPTY")
+    @NotNull(message = "NAME_EMPTY")
+    @NotBlank(message = "NAME_EMPTY")
     String name;
-    @NotNull(message = "NOT_EMPTY")
-    @NotBlank(message = "NOT_EMPTY")
+    @NotNull(message = "DESCRIPTION_NOT_NULL")
+    @NotBlank(message = "DESCRIPTION_NOT_NULL")
     String description;
     @NotNull(message = "NOT_NULL")
     @Min(value = 1, message = "GREATER_NUMBER")
     double price;
+    @NotNull(message = "CATEGORY_NOT_NULL")
     Long dishCategoryId;
-    @NotNull(message = "NOT_EMPTY")
-    @NotBlank(message = "NOT_EMPTY")
+    @NotNull(message = "IMAGE_NOT_NULL")
+    @NotBlank(message = "IMAGE_NOT_NULL")
     String imageUrl;
+    @NotNull(message = "UNIT_NOT_NULL")
     Long unitId;
+    @NotNull(message = "RESTAURANT_NOT_EXISTED")
     Long restaurantId;
 }
